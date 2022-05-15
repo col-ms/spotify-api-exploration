83db2ceb497afa597ced0cf9f2b02d253c93b51d
Collin Smith <collinmsmith22@gmail.com>
2022-05-15 17:15:53 -0400

    Data retrieval function.

--- data_gathering.R
+++ data_gathering.R
@@ -2,6 +2,9 @@
 # regarding Mac Miller's discography, as
 # it currently exists on Spotify.
 
+# Loads any user-defined functions stored in file, and
+# performs necessary authentication for API access
 source('functions.R')
 
-authenticate()
+# retrieve discography
+mm_disco <- get_artist_audio_features(artist = 'Mac Miller')
