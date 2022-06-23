12dd840f9bb0415905c0d810944e45e30b80aa72
Collin Smith <collinmsmith22@gmail.com>
2022-06-23 13:32:35 -0400

    streamlined api auth

--- auth_func.R
+++ auth_func.R
@@ -1,13 +1,6 @@
 # authentication function
 
 authenticate <- function(){
-
-  # Authentication to allow access to Spotify API
-  Sys.setenv(SPOTIFY_CLIENT_ID = 
-               readr::read_lines("spotify_dev_info.txt")[1])
-  
-  Sys.setenv(SPOTIFY_CLIENT_SECRET = 
-               readr::read_lines("spotify_dev_info.txt")[2])
   
   # Store access token for use in API call functions
   access_token = spotifyr::get_spotify_access_token()
